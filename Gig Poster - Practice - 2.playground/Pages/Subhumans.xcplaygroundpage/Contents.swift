76d99f4ae358827e11a80159e7ff82203a2ffb59
Wesley Sun <wsun@lcs.on.ca>
2020-02-28 12:57:24 -0500

    25 - 162 = -147 get the position of the last shape

--- Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
+++ Gig Poster - Practice - 2.playground/Pages/Subhumans.xcplaygroundpage/Contents.swift
@@ -51,7 +51,7 @@ canvas.drawShapesWithFill = true
 
 for verticalPosition in stride(from: 50, through: 225, by: 175) {
     
-for horizontalPosition in stride(from: 25, through: 25 + 2 * 162, by: 162) {
+for horizontalPosition in stride(from: -137, through: 25 + 2 * 162, by: 162) {
         
         var vertices : [Point] = [] // empty array
         vertices.append(Point(x: horizontalPosition, y: verticalPosition))     // 1
